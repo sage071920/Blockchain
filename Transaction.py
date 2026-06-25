24769a55c76bd71e5cb4a7d3abd474e045ae15b5
sage071920 <samuel.henze@gmx.de>
2026-06-25 11:08:35 +0000

    Add transaction handling methods to Transaction class

--- Transaction.py
+++ Transaction.py
@@ -1,12 +1,44 @@
+from ecdsa import VerifyingKey, SECP256k1, BadSignatureError
+
 import hashlib
 
+
 class Transaction:
-
     def __init__(self, sender: str | None, recipient: str, amount: float):
-        pass
+        self.sender = sender
+        self.recipient = recipient
+        self.amount = amount
+        self.signature: str | None = None
 
-    def to_dict(self):
-        pass #TODO
+    def transaction_hash(self) -> str:
 
-    def is_valid(self):
-        pass #TODO
+        trans_str = f"{self.sender}{self.recipient}{self.amount}"
+
+        trans_bin = trans_str.encode()
+
+        return hashlib.sha256(trans_bin).hexdigest()
+
+    def is_valid(self) -> bool:
+
+        if self.sender is None:
+            return True
+
+        if self.signature is None:
+            return False
+
+        vk = VerifyingKey.from_string(bytes.fromhex(self.sender), curve=SECP256k1)
+
+        try:
+            vk.verify(bytes.fromhex(self.signature), self.transaction_hash().encode())
+            return True
+
+        except BadSignatureError:
+            return False
+
+    def to_dict(self) -> dict:
+        return {
+            "sender": self.sender,
+            "recipient": self.recipient,
+            "amount": self.amount,
+            "signature": self.signature,
+        }
